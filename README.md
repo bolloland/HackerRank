2951fe11f6f954e1b93340d86ae5b5b806806e3a
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-04-05 23:20:02 -0500

    plus_minus complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HackerRank
+
+1 Month Preparation Kit
---
+++ Week_1/01_Plus_Minus/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    console.log((arr.filter(num => num > 0).length / arr.length).toFixed(6))
+    console.log((arr.filter(num => num < 0).length / arr.length).toFixed(6))
+    console.log((arr.filter(num => num === 0).length / arr.length).toFixed(6))
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
